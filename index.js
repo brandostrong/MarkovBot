26db68c2cfa755b6464d509a67738654b641ebbe
Brandon Strong <me@brandostrong.com>
2021-09-13 17:04:35 -0700

    initial

---
+++ index.js
@@ -0,0 +1,30 @@
+const { channelMention } = require('@discordjs/builders')
+const { Client, Collection, Intents, Channel } = require('discord.js')
+const { token } = require('./config.json')
+const fs = require('fs')
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+client.commands = new Collection()
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command)
+}
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'))
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`)
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args))
+  } else {
+    client.on(event.name, (...args) => event.execute(...args))
+  }
+}
+
+
+// Login to Discord with your client's token
+client.login(token)
